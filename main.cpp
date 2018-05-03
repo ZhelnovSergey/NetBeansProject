30de1e8e5868e3a06ad38fca87b3e50c877951cc
sergey <sergey@zhelnov>
2018-05-03 16:51:16 +0300

    Second Commit: Added "Hello World" to code

--- main.cpp
+++ main.cpp
@@ -12,14 +12,14 @@
  */
 
 #include <cstdlib>
+#include <stdio.h>
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
 
+int main(int argc, char** argv) 
+{
+    printf("Hello World!\n");
     return 0;
 }
 
